036f9d3d5b283f83f09a447fe4e2b60dfafb6274
Kylie Le <kylie.tram.le@gmail.com>
2016-08-08 13:41:59 -0700

    built panel controller for tabs

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,20 +4,29 @@
   app.controller('StoreController', function() {
     this.products = gems; // property of the controller
   });
+  
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    });
 
   var gems = [
 
   {
     name: 'Dodecahedron',
     price: 2,
-    description: '...',
+    description: 'A very pretty gem',
     canPurchase: true,
     soldOut: true,
-    image:
-      {
-        full: '<%= asset_path("dodecahedron.jpg") %>',
-        thumb: ''
-      }
+    image: 'http://www.diamondcrystals.net/africa/gem1t.jpg',
   },
 
   {
